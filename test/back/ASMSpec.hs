f72fc4e0338ff0efc2472d2ab84116be9edacf6e
Albie Baker-Smith <bakersmitha@gmail.com>
2018-10-31 16:45:15 +0000

    added pop spec

--- test/back/ASMSpec.hs
+++ test/back/ASMSpec.hs
@@ -11,6 +11,7 @@ sp = 14
 asmSpec :: Spec
 asmSpec = describe "asm generation" $ do
     pushSpec
+    popSpec
 
 pushSpec :: Spec
 pushSpec = describe "push" $ do
@@ -19,3 +20,11 @@ pushSpec = describe "push" $ do
                  , StoreIdx { r=3, base=sp, offset=1 }
                  , AddI sp sp 2]
         runSt empty (push [1, 3]) `shouldBe` exp
+
+popSpec :: Spec
+popSpec = describe "pop" $ do
+    it "generates asm" $ do
+        let exp = [LoadIdx { r=1, base=sp, offset=(-1)}
+                 , LoadIdx { r=3, base=sp, offset=(-2)}
+                 , SubI sp sp 2]
+        runSt empty (pop [1, 3]) `shouldBe` exp
