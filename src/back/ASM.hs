9c8c45ea80742811b9c826ba9d20d950b5d4d840
Albie Baker-Smith <bakersmitha@gmail.com>
2018-10-31 15:54:17 +0000

    asm

--- src/back/ASM.hs
+++ src/back/ASM.hs
@@ -1,4 +1,20 @@
 module Back.ASM where
 
+import qualified Front.AST as AST
 import Back.Env
 import Back.Instr
+
+stm :: AST.Stm -> St [Instr]
+stm (AST.Return val) = undefined
+stm (AST.Def name val) = undefined
+stm (AST.Assign name val) = undefined
+stm (AST.AssignArr name arrIdx val) = undefined
+stm (AST.If cond body) = undefined
+stm (AST.IfElse cond sThen sElse) = undefined
+stm (AST.For name range body) = undefined
+stm (AST.While cond body)= undefined
+stm (AST.Call func) = undefined
+stm (AST.Comp s1 s2) = undefined
+stm (AST.NoOp) = undefined
+stm (AST.Print val) = undefined
+stm (AST.PrintLn) = undefined
--- src/back/Env.hs
+++ src/back/Env.hs
@@ -25,3 +25,23 @@ data Env = Env {
     -- Used to generate fresh labels for branch locations.
   , currLabel :: Int
 }
+
+-- Uses ASM environment to compute value.
+type St a = State Env a
+
+-- Keeps track of a variable and associated stack address.
+addVar :: VarName -> Addr -> St ()
+addVar name addr = modify $ \env ->
+    env { varAddr = Map.insert name addr (varAddr env) }
+
+-- Sets the arguments of the function ASM is currently being generated for.
+setArgs :: [VarName] -> St ()
+setArgs args = modify $ \env ->
+    env { args = Map.fromList (zip args [0..]) }
+
+-- Returns a fresh label to use as a branch location.
+freshLabel :: St String
+freshLabel = do
+    env <- get
+    put (env { currLabel = (currLabel env) + 1 })
+    return (show (currLabel env))
